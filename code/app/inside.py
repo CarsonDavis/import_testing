bb1373dd694971fcd9333b68a7d16267612bbee3
Ashish Acharya <ashish.acharya14@gmail.com>
2020-06-03 14:20:45 -0500

    add try expect to imports

--- code/app/inside.py
+++ code/app/inside.py
@@ -1,3 +1,6 @@
-from .inside_helper import desired_value
+try:
+    from .inside_helper import desired_value
+except:
+    from inside_helper import desired_value
 
 print("print from code/app/inside.py")
